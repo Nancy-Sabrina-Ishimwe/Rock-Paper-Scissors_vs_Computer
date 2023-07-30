d1d65a2e202cecabfaf766b110976a0e580c5d35
Nancy-Sabrina-Ishimwe <ishimwenancysabrina1@gmail.com>
2023-07-30 03:12:12 +0200

    functions

--- index.js
+++ index.js
@@ -1,65 +1,47 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
+const playerSelection = document.querySelector("#playerSelection");
+const computerSelection = document.querySelector("#computerSelection");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
 
-let userChoice
-let computerChoice
-let result
+choiceBtns.forEach(button => button.addEventListener("click", () => {
 
-possibleChoices.forEach(possibleChoice => possibleChoice. addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-    getResult()
-}))
+    player = button.textContent;
+    getComputerChoice();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}));
 
-function generateComputerChoice() {
+function getComputerChoice(){
 
-    const randomNumber = Math.floor( Math.random() * 3)
+    const randNum = Math.floor(Math.random() * 3) + 1;
 
-    if(randomNumber ===1) {
-        computerChoice = 'rock'
+    switch(randNum){
+      case 1:
+        computer = "ROCK";
+        break;
+      case 2:
+        computer = "PAPER";
+        break;
+      case 3:
+        computer = "SCISSORS";
+        break;
     }
-
-    if(randomNumber ===2) {
-        computerChoice = 'scissors'
+}
+function checkWinner(){
+    if(player == computer){
+      return "Draw!";
     }
-
-    if(randomNumber ===1) {
-        computerChoice = 'paper'
+    else if(computer == "ROCK"){
+      return (player == "PAPER") ? "You Win!" : "You Lose!"
     }
-
-    computerChoiceDisplay.innerHTML = computerChoice
-     function getResult() {
-         if (computerChoice === userChoicehoice){
-            result ='its a draw!'
-
-         }
-         if (computerChoice === 'rock' && userChoice === "paper"){
-            result ='you win!'
-
-         }
-         if (computerChoice === 'rock' && userChoice === "scissors"){
-            result ='you lost!'
-
-         }
-         if (computerChoice === 'paper' && userChoice === "scissors"){
-            result ='you win!'
-
-         }
-         if (computerChoice === 'paper' && userChoice === "rock"){
-            result ='you lose!'
-
-         }
-         if (computerChoice === 'scissors' && userChoice === "paper"){
-            result ='you win!'
-
-         }
-         if (computerChoice === 'scissors' && userChoice === "paper"){
-            result ='you lose!'
-
-         }
-         resultDisplay.innerHTML= result
-     }
+    else if(computer == "PAPER"){
+      return (player == "SCISSORS") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "SCISSORS"){
+      return (player == "ROCK") ? "You Win!" : "You Lose!"
+    }
 }
