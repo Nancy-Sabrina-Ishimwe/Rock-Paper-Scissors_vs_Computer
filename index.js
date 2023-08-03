ff7598dbe050806d9e3ed714a5f98ae8bb4b154b
Nancy-Sabrina-Ishimwe <ishimwenancysabrina1@gmail.com>
2023-08-03 11:40:01 +0200

    made changes and referred on the odin project

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
-const playerSelection = document.querySelector("#playerText");
-const computerSelection = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const choiceBtns = document.querySelectorAll(".btn");
-let player;
-let computer;
-let result;
-
-choiceBtns.forEach(button => button.addEventListener("click", () => {
-
-    player = button.textContent;
-    getComputerChoice();
-    playerText.textContent = `Player: ${player}`;
-    computerText.textContent = `Computer: ${computer}`;
-    resultText.textContent = checkWinner();
-}));
-
-function getComputerChoice(){
-
-    const randNum = Math.floor(Math.random() * 3) + 1;
-
-    switch(randNum){
-      case 1:
-        computer = "ROCK";
-        break;
-      case 2:
-        computer = "PAPER";
-        break;
-      case 3:
-        computer = "SCISSORS";
-        break;
+const getplayerSelection = userInput => {
+  userInput = userInput.toLowerCase(); 
+   if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+     return userInput;
+   } else {
+     console.log('Error!');
+   }
+ };
+ 
+ function getComputerChoice() {
+  switch (Math.floor(Math.random() * 3)) {
+     case 0:
+       return 'rock';
+     case 1:
+       return 'paper';
+     case 2:
+       return 'scissors';
+     }
+ };
+ 
+ 
+  
+  function playRound(playerSelection, computerSelection) {
+      if (playerSelection === computerSelection) {
+        return 'The game is a tie!';
+      } 
+        
+      else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+           return 'You Lose! Paper beats Rock!';
+        } else {
+           return 'You won!';
+       } 
+      }
+      
+      else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+          return 'You lose rock beats scissors!';
+        } else {
+          return 'You won!';
+        }
+      } 
+  
+  else if (playerSelection === 'paper') {
+    if (computerSelection === 'scissors') {
+      return 'You lose scissors beats papers!';
+    } else {
+      return 'You won!';
+      
     }
-}
-function checkWinner(){
-    if(player == computer){
-      return "Draw!";
-    }
-    else if(computer == "ROCK"){
-      return (player == "PAPER") ? "You Win!" : "You Lose!"
-    }
-    else if(computer == "PAPER"){
-      return (player == "SCISSORS") ? "You Win!" : "You Lose!"
-    }
-    else if(computer == "SCISSORS"){
-      return (player == "ROCK") ? "You Win!" : "You Lose!"
-    }
-}
+   }
+  }
+  const Game = () => {
+      let playerSelection = getplayerSelection(prompt());
+      const computerSelection = getComputerChoice();
+    console.log('You threw: ' + playerSelection);
+    console.log('The computer threw: ' + computerSelection);
+    console.log(playRound(playerSelection,computerSelection));
+  }
+  Game();
